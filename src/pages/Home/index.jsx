ed6fc2e0c36c717b0bd680e2d1e28f9c590e1cdd
Rim-sherif <120836789+Rim-sherif@users.noreply.github.com>
2025-03-12 23:30:52 +0200

    new push

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,15 +2,15 @@ import { faBook, faCertificate, faGraduationCap, faUsers } from '@fortawesome/fr
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import img from '/learning-concept-illustration.png';
-// import Featuredinstructors from '../../components/Featuredinstractors';
-// import img1 from '../../assets/images/html-css-collage-concept.jpg';
-// import img2 from '../../assets/images/uiux.jpg';
-// import img3 from '../../assets/images/business-people-blue-background.jpg';
-// import img4 from '../../assets/images/usman-yousaf-6pmG8XIKE2w-unsplash.jpg';
-// import img5 from '../../assets/images/3409297.jpg';
-// import img6 from '../../assets/images/top-view-photography-accesories-with-copy-space.jpg';
-// import img7 from '../../assets/images/SL.123119.26540.04.jpg';        
-// import img8 from '../../assets/images/man-jumping-impossible-possible-cliff-sunset-background-business-concept-idea.jpg';
+import Featuredinstructors from '../../components/Featuredinstractors';
+import img1 from '../../assets/images/html-css-collage-concept.jpg';
+import img2 from '../../assets/images/uiux.jpg';
+import img3 from '../../assets/images/business-people-blue-background.jpg';
+import img4 from '../../assets/images/usman-yousaf-6pmG8XIKE2w-unsplash.jpg';
+import img5 from '../../assets/images/3409297.jpg';
+import img6 from '../../assets/images/top-view-photography-accesories-with-copy-space.jpg';
+import img7 from '../../assets/images/SL.123119.26540.04.jpg';        
+import img8 from '../../assets/images/man-jumping-impossible-possible-cliff-sunset-background-business-concept-idea.jpg';
 
 
 const Home = () => {
@@ -76,7 +76,7 @@ const Home = () => {
             </section>
 
    
-        {/* <section className="py-20  relative overflow-hidden">
+        <section className="py-20  relative overflow-hidden">
             <div className="container mx-auto px-4">
                 <h2 className="text-4xl font-bold text-center mb-16 text-[#A5158C] relative after:content-[''] after:absolute after:bottom-[-12px] after:left-1/2 after:-translate-x-1/2 after:w-24 after:h-1 after:bg-white">
                     Explore Popular Categories
@@ -145,7 +145,7 @@ const Home = () => {
                     ))}
                 </div>
             </div>
-        </section> */}
+        </section>
 
                     {/* Featured Instructors */}
           <Featuredinstructors/>

195b2d1e6155ce1bf1cd4da0158d3fa812c19e2a
Abdulrahmanfawzy <abdulrahman2d77@gmail.com>
2025-03-14 19:47:12 +0200

    add filteration in instructors page

--- src/pages/Instructors/index.jsx
+++ src/pages/Instructors/index.jsx
@@ -7,11 +7,11 @@ import { Link } from "react-router-dom";
 
 export default function Instructors() {
   const [users, setUsers] = useState([]);
+  const [selectedCategories , setSelectedCategories] = useState([]);
+  const [originalUsers, setOriginalUsers] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-  const [category, setCategory] = useState([]);
-
-  const categories = [
+  const [categories, setCategories] = useState([
     { id: "1", category: "Business and Entrepreneurship" },
     { id: "2", category: "Health and Nutrition" },
     { id: "3", category: "Science & Environment" },
@@ -22,7 +22,9 @@ export default function Instructors() {
     { id: "8", category: "Art, Design & Media" },
     { id: "9", category: "Career Readiness" },
     { id: "10", category: "Languages" },
-  ];
+  ]);
+  const [filter , setFilter] = useState([]);
+  const [close , setClose] = useState(false);
 
   const stars = [1, 2, 3, 4, 5];
 
@@ -32,8 +34,8 @@ export default function Instructors() {
       const { data } = await axios.get(
         `${import.meta.env.VITE_BASE_URL}/api/v1/instructors`
       );
-      console.log(data);
       setUsers(data?.data);
+      setOriginalUsers(data?.data);
       setLoading(false);
       setError("");
     } catch (error) {
@@ -44,27 +46,83 @@ export default function Instructors() {
   useEffect(() => {
     getAllInstructors();
   }, []);
+  
+  const handleCategory = (item ,e)=>{
+    let updatedCategories = [...selectedCategories];
+
+    if(e.target.checked){
+      updatedCategories.push(item);
+    }else{
+      updatedCategories = updatedCategories.filter(el=>el != item);
+    }
+
+    setSelectedCategories(updatedCategories);
+
+    if(updatedCategories.length > 0){
+      setUsers(originalUsers.filter(user=> updatedCategories.includes(user.category)))
+    }else{
+      setUsers(originalUsers)
+    }
+
+  }
+
+  const closeFun = (e)=>{
+    setClose(!close)
+  }
+
 
   return (
     <div className="flex w-[90%] mx-auto justify-between">
-      <div className="w-[23%] py-10 px-3">
+      <div style={{transition: "1s" , zIndex: 222}} className={`w-[50%] z-30 md:w-[23%] px-10 py-10 md:px-3 fixed h-[100vh] md:left-0 md:static md:bg-[transparent] bg-white ${close ? 'left-[0%]' : 'left-[-50%]'}`}>
+        
+        <div onClick={closeFun} className="close block md:hidden absolute top-2 right-[-30px] bg-blue-300 py-1 px-3">
+          {close ? "X" : "B"}
+        </div>
+       
+        {/* <div style={{zIndex: 1}} className=" fixed inset-0 bg-[rgba(0,0,0,0.5)]"></div> */}
+
         {categories
           ? categories.map((item) => (
-              <div key={item.id}>
-                <input type="checkbox" />
-                <label htmlFor="">{item.category}</label>
-              </div>
+            <div key={item.id} className="flex items-center gap-3 mb-3">
+            <input
+              type="checkbox"
+              onChange={(e)=>handleCategory(item.category , e)}
+              id={`checkbox-${item.id}`}
+              className="hidden peer border-gray-300"
+            />
+            <div className="w-5 h-5 border-2 border-gray-400 rounded-md flex items-center justify-center transition-all duration-300 peer-checked:bg-blue-500 peer-checked:border-blue-500">
+              <svg
+                className="w-4 h-4 text-white opacity-0 transition-all duration-200 peer-checked:opacity-100"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <polyline points="20 6 9 17 4 12"></polyline>
+              </svg>
+            </div>
+            <label
+              htmlFor={`checkbox-${item.id}`}
+              className="cursor-pointer text-gray-700 font-medium peer-checked:text-blue-600 transition-all duration-300"
+            >
+              {item.category}
+            </label>
+          </div>
+          
             ))
           : ""}
       </div>
 
-      <div className="w-[75%] my-10 flex flex-wrap justify-between">
+      <div className="w-[100%] lg:w-[75%] my-10 flex flex-wrap justify-between">
         {error ? <div>{error}</div> : ""}
 
         {users ? (
           users.map((user) => (
             <div
-              className="w-[32.5%] border-1 rounded border-gray-200 p-5 mb-3 text-center"
+              className="lg:w-[32.5%] w-[100%] sm:w-[48%] border-1 rounded border-gray-200 p-5 mb-3 text-center"
               key={user._id}
             >
               <div className="mb-3">
@@ -100,7 +158,7 @@ export default function Instructors() {
                 </section>
               </div>
               <Link to="/">
-                <button className="bg-[]">Profile</button>
+                <button className="bg-[#0194FE] cursor-pointer text-white block w-full py-2 rounded mt-3">Profile</button>
               </Link>
             </div>
           ))

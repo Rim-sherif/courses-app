8de3570d67a5b7042e8a5690d546a87a40b7a25f
Abdulrahmanfawzy <abdulrahman2d77@gmail.com>
2025-03-24 21:43:14 +0200

    updates in paid and free

--- src/pages/Dashboard/CourseController/FreeCourses.jsx
+++ src/pages/Dashboard/CourseController/FreeCourses.jsx
@@ -1,50 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import img3 from "../../../assets/images/business-people-blue-background.jpg";
-import img1 from "../../../assets/images/html-css-collage-concept.jpg";
-import img2 from "../../../assets/images/uiux.jpg";
-import img4 from "../../../assets/images/usman-yousaf-6pmG8XIKE2w-unsplash.jpg";
 import { toast } from "react-toastify";
 import axios from "axios";
-import CourseCard from "../../../components/courseCard/CourseCard";
 import CourseItem from "../../../components/CourseItem";
 
 const FreeCourses = () => {
   const navigate = useNavigate();
-  // const freeCourses = [
-  //   {
-  //     id: 1,
-  //     title: "HTML & CSS Basics",
-  //     image: img1,
-  //     duration: "6h",
-  //     level: "Beginner",
-  //     students: 1234,
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "UI/UX Design",
-  //     image: img2,
-  //     duration: "8h",
-  //     level: "Intermediate",
-  //     students: 890,
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "Business Management",
-  //     image: img3,
-  //     duration: "10h",
-  //     level: "Advanced",
-  //     students: 756,
-  //   },
-  //   {
-  //     id: 4,
-  //     title: "Medical Sciences",
-  //     image: img4,
-  //     duration: "12h",
-  //     level: "Intermediate",
-  //     students: 543,
-  //   },
-  // ];
   const [freeCourses , setFreeCourses] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [levelFilter, setLevelFilter] = useState("All");
@@ -56,8 +17,6 @@ const FreeCourses = () => {
       const id = window.localStorage.getItem("instructor_id");
       const {data} = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/v1/user/${id}`);
       const coursesFree = data?.instructor?.courses.filter(course=>course.access_type == "free");
-      console.log(coursesFree);
-      
       setFreeCourses(coursesFree);
     } catch (error) {
       toast.error(error.message)
@@ -105,45 +64,13 @@ const FreeCourses = () => {
         </div>
         </div>
 
-        <div className="flex flex-wrap justify-between">
+        <div className="grid gap-4  
+            grid-cols-1  
+            sm:grid-cols-2  
+            md:grid-cols-3  
+            lg:grid-cols-3  
+            xl:grid-cols-4">
           {freeCourses.length > 0 && freeCourses.map(course=><CourseItem key={course._id} course={course} />)}
-          {/* {filteredCourses.map((course) => (
-            <div
-              key={course.id}
-              className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow"
-            >
-              <img
-                src={course.image}
-                alt={course.title}
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-4">
-                <h3 className="text-lg font-semibold text-gray-800 mb-2">
-                  {course.title}
-                </h3>
-                <div className="flex items-center justify-between text-sm text-gray-600 mb-2">
-                  <span className="flex items-center">
-                    <i className="fas fa-clock mr-2"></i>
-                    {course.duration}
-                  </span>
-                  <span className="flex items-center">
-                    <i className="fas fa-signal mr-2"></i>
-                    {course.level}
-                  </span>
-                </div>
-                <div className="flex items-center text-sm text-gray-600">
-                  <i className="fas fa-users mr-2"></i>
-                  {course.students.toLocaleString()} students
-                </div>
-                <button 
-                  onClick={() => navigate(`/dashboard/courses/${course.id}`, { state: { course, type: 'free' } })}
-                  className="mt-4 w-full bg-[#410445] hover:bg-[#402841] text-white py-2 px-4 rounded-lg transition-colors"
-                >
-                  View
-                </button>
-              </div>
-            </div>
-          ))} */}
         </div>
       </div>
     </div>
--- src/pages/Dashboard/CourseController/PaidCourses.jsx
+++ src/pages/Dashboard/CourseController/PaidCourses.jsx
@@ -270,7 +270,12 @@ const FreeCourses = () => {
         </div>
         </div>
 
-        <div className="flex flex-wrap justify-between">
+        <div className="grid gap-4  
+            grid-cols-1  
+            sm:grid-cols-2  
+            md:grid-cols-3  
+            lg:grid-cols-3  
+            xl:grid-cols-4">
           {freeCourses.length > 0 && freeCourses.map(course=><CourseItem key={course._id} course={course} />)}
           {/* {filteredCourses.map((course) => (
             <div
